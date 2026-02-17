d8098e5561eefe8c945eaf3fab6c7cc12b7de695
Dhruv-C-Tripathi <dreektseayger@gmail.com>
2026-02-18 00:36:24 +0530

    error resolution

--- memoryAndChatbot.py
+++ memoryAndChatbot.py
@@ -26,7 +26,7 @@ chain = prompt | llm
 
 store = {}
 
-def get_session_history(session_id: str) -> InMemoryChatMessageHistoryhi:
+def get_session_history(session_id: str) -> InMemoryChatMessageHistory:
     """Retrieve the message history for a given session ID."""
     if session_id not in store:
         store[session_id] = InMemoryChatMessageHistory()
